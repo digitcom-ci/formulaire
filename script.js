83ea9d865c6d43c2f26db1c24f475067e8bed9ca
Doumbia <dd.somanager@gmail.com>
2026-01-30 04:37:03 +0000

    Update script.js

--- script.js
+++ script.js
@@ -8,29 +8,23 @@ form.addEventListener("submit", function(e) {
     button.classList.add("loading");
     button.disabled = true;
 
-    grecaptcha.ready(function() {
-        grecaptcha.execute("gq5FClY23NZd4D-T9", {action: "submit"}).then(function(token) {
+    const params = {
+        nom: document.getElementById("nom").value,
+        numero: document.getElementById("numero").value,
+        entreprise: document.getElementById("entreprise").value || "Non renseignée",
+        email: document.getElementById("email").value || "Non renseigné"
+    };
 
-            const params = {
-                nom: document.getElementById("nom").value,
-                numero: document.getElementById("numero").value,
-                entreprise: document.getElementById("entreprise").value || "Non renseignée",
-                email: document.getElementById("email").value || "Non renseigné",
-                captcha: token
-            };
-
-            emailjs.send("service_40476la", "template_3a1cizm", params)
-                .then(() => {
-                    successMessage.style.display = "block";
-                    setTimeout(() => {
-                        window.location.href = "document.pdf";
-                    }, 1200);
-                })
-                .catch(() => {
-                    alert("Erreur lors de l’envoi. Réessayez.");
-                    button.classList.remove("loading");
-                    button.disabled = false;
-                });
+    emailjs.send("service_40476la", "template_3a1cizm", params)
+        .then(() => {
+            successMessage.style.display = "block";
+            setTimeout(() => {
+                window.location.href = "document.pdf";
+            }, 1200);
+        })
+        .catch(() => {
+            alert("Erreur lors de l’envoi. Réessayez.");
+            button.classList.remove("loading");
+            button.disabled = false;
         });
-    });
 });
