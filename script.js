d7aafb225d3de74cb8d133c33169cad3ac508db0
Doumbia <dd.somanager@gmail.com>
2026-01-30 05:19:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -54,7 +54,6 @@ document.addEventListener('DOMContentLoaded', function() {
             .finally(function() {
                 resetButton();
             });
-            
     });
 
     function showMessage(text, type) {
